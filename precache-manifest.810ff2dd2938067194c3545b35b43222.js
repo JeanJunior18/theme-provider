0d576dcf73fe677431752338647ff5e0ebc6c0b5
Jean Junior <jclgjunior@gmail.com>
2020-09-07 15:56:02 -0300

    Updates

---
+++ precache-manifest.810ff2dd2938067194c3545b35b43222.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bd4c0f073e767f73bfa12608c97055b",
+    "url": "/theme-provider/index.html"
+  },
+  {
+    "revision": "51b30d6ad5c0cfd759fc",
+    "url": "/theme-provider/static/js/2.07ec216b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/theme-provider/static/js/2.07ec216b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9397273e5b1f0a0c5bbb",
+    "url": "/theme-provider/static/js/main.dbe15f12.chunk.js"
+  },
+  {
+    "revision": "cb524f24a4381fae948e",
+    "url": "/theme-provider/static/js/runtime-main.1e23e60d.js"
+  }
+]);
